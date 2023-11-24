2d2fc59bd8ba2846abfd6b1633ef79beedf73578
Rajalaxmi2002 <choudhuryrajalaxmi8@gmail.com>
2023-11-24 09:58:38 +0530

    Added more components

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,12 +6,26 @@ class Notes(models.Model):
     title = models.CharField(max_length=200) 
     description = models.TextField()
     
-    def __str__(self):  # Corrected double underscores here
+    def __str__(self):  
         return self.title
 
     class Meta:
         verbose_name = "note"
         verbose_name_plural = "notes"
 
-    
-   
+class Homework(models.Model):  
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)  
+    title = models.CharField(max_length=100)  
+    description = models.TextField()
+    due = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)
+      
+    def __str__(self):  
+        return self.title
+class Todo(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    is_finished = models.BooleanField(default=False)
+    def __str__(self):  
+        return self.title
